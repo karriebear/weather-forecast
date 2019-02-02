55b11d6da04a3e9cedc12baf032fc0d754b8ec62
karriebear <karrie@kinnek.com>
2019-02-01 23:01:05 -0500

    Updates

---
+++ precache-manifest.e767e9da853d43be8188910db7016987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d38ab6dc1f45e8eef0",
+    "url": "/weather-forecast/static/js/runtime~main.f2d38ab6.js"
+  },
+  {
+    "revision": "dfbfbec4b4eb94f8fdfd",
+    "url": "/weather-forecast/static/js/main.dfbfbec4.chunk.js"
+  },
+  {
+    "revision": "a34df6311f944226eeb5",
+    "url": "/weather-forecast/static/js/1.a34df631.chunk.js"
+  },
+  {
+    "revision": "dfbfbec4b4eb94f8fdfd",
+    "url": "/weather-forecast/static/css/main.ab4941b2.chunk.css"
+  },
+  {
+    "revision": "ef16f016db58d868e6dc3f60249fa71d",
+    "url": "/weather-forecast/index.html"
+  }
+];
